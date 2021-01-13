b2440a8f0af27100870619be6790842eab291d4b
nicolas <nicolas.diarra@stellar.io>
2021-01-13 21:57:48 +0100

    feat(#5): Add note feature is fixed

--- src/Builder/Note/NoteDirector.php
+++ src/Builder/Note/NoteDirector.php
@@ -11,25 +11,24 @@ class NoteDirector
 {
     public function buildNote(Hub $hub, User $user, $data)
     {
-//        dump(empty($data->getNoteTitle())); die();
         $note = new Note();
 
-        if(empty($data->getNoteTitle())) {
-            $data->setNoteTitle($data->getNoteTitle());
+        if(!empty($data->getNoteTitle())) {
+            $note->setNoteTitle($data->getNoteTitle());
         }
-        if(empty($data->getNoteContent())) {
-            $data->setNoteContent($data->getNoteContent());
+        if(!empty($data->getNoteContent())) {
+            $note->setNoteContent($data->getNoteContent());
         }
-        if(empty($data->getNoteFirstLink())) {
-            $data->setNoteFirstLink($data->getNoteFirstLink());
+        if(!empty($data->getNoteFirstLink())) {
+            $note->setNoteFirstLink($data->getNoteFirstLink());
         }
-        if(empty($data->getNoteSecondLink())) {
-            $data->setNoteSecondLink($data->getNoteSecondLink());
+        if(!empty($data->getNoteSecondLink())) {
+            $note->setNoteSecondLink($data->getNoteSecondLink());
         }
-        if(empty($data->getNoteThirdLink())) {
-            $data->setNoteThirdLink($data->getNoteThirdLink());
+        if(!empty($data->getNoteThirdLink())) {
+            $note->setNoteThirdLink($data->getNoteThirdLink());
         }
-//        $data->setCreationDatetime(new DateTime());
+        $note->setCreationDatetime(new DateTime());
 //      $anime->setstartDate(new DateTime($data['startDate']));
         $note->setCreator($user);
         $note->setHub($hub);
