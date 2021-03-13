d94e5eb088713484e4f1d8d59c3a212f07583187
nicolas <nicolas.diarra@stellar.io>
2021-03-13 14:08:34 +0100

    feat(#24): Use date fns

--- assets/app/components/hubs/show/Hub.vue
+++ assets/app/components/hubs/show/Hub.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <div class="box">Hub creator <strong>{{ hub.name }}</strong></div>
+    <div class="columns">
+      <div class="column is-12">
+        <section class="hero">
+          <div class="hero-body pl-0 pr-0">
+            <p class="title">
+              {{ isHubUser ? 'Our hub' : 'Hub of' }} {{ hub.creator ? hub.creator.username : ''}}
+              <span class="is-pulled-right"></span>
+            </p>
+            <p class="subtitle">
+              <span v-if="hub.creationDatetime"><small> Created on {{ new Date(hub.creationDatetime) | date }}</small></span>
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+
     <div v-if="notes.length > 0">
       <note-list :notes="notes" layout="grid"/>
     </div>
+
     <div v-else-if="isNoteLoading && !notes.length > 0" class="has-text-centered">
       <i class="fas fa-spinner fa-pulse fa-2x fa-fw"/>
     </div>
@@ -21,6 +37,7 @@ import NoteModule from '../../../store/modules/note'
 import HubModule from '../../../store/modules/hub'
 
 import NoteList from '../../notes/layout/NoteList.vue'
+import UserModule from "../../../store/modules/user";
 
 @Component({
   components: { NoteList }
@@ -33,6 +50,10 @@ export default class Hub extends Vue {
     await getModule(HubModule, this.$store).getHub(this.hubId)
   }
 
+  get isHubUser (): boolean {
+    return this.hubId === this.userHubId
+  }
+
   get notes () {
     return getModule(NoteModule, this.$store).notes
   }
@@ -43,6 +64,10 @@ export default class Hub extends Vue {
 
   get hub () {
     return getModule(HubModule, this.$store).hub
+  }
+
+  get userHubId () {
+    return getModule(UserModule, this.$store).userHubId
   }
 
 }
